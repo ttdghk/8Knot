3f3535807d3e28634d463bf47b4eaa7a8706948c
Sean P. Goggins <s@goggins.com>
2024-01-26 13:50:41 -0600

    explorer_pr_assignments documentation update

--- docs/materialized_views/explorer_pr_assignments.sql
+++ docs/materialized_views/explorer_pr_assignments.sql
@@ -2,16 +2,22 @@
 
 SELECT
     pr.pull_request_id,
-    pr.repo_id AS id,
+    pr.repo_id AS ID,
     pr.pr_created_at AS created,
-    pr.pr_closed_at as closed,
+    pr.pr_closed_at AS closed,
     pre.created_at AS assign_date,
-    pre.action AS assignment_action,
-    pre.cntrb_id AS assignee
+    pre.ACTION AS assignment_action,
+    pre.cntrb_id AS assignee,
+    pre.node_id AS node_id 
 FROM
-    pull_requests pr
-LEFT OUTER JOIN
-    pull_request_events pre
-ON
-    pr.pull_request_id = pre.pull_request_id AND
-    pre.action IN ('unassigned', 'assigned')
+    (
+        augur_data.pull_requests pr
+        LEFT JOIN augur_data.pull_request_events pre ON (
+            (
+                ( pr.pull_request_id = pre.pull_request_id ) 
+                AND (
+                    ( pre.ACTION ) :: TEXT = ANY ( ARRAY [ ( 'unassigned' :: CHARACTER VARYING ) :: TEXT, ( 'assigned' :: CHARACTER VARYING ) :: TEXT ] ) 
+                ) 
+            ) 
+        ) 
+    )
